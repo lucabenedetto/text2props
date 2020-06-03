7cbedb283ff45270a6f7b6fa01630afa172bd327
Luca Benedetto <luca@benedettoluca.com>
2020-06-03 11:39:25 +0200

    update math utils, refactoring skill and dff in inf function

--- text2props/utils/math.py
+++ text2props/utils/math.py
@@ -2,7 +2,7 @@ import numpy as np
 from ..constants import DISCRIMINATION_COEFFICIENT
 
 
-def item_response_function(difficulty, skill, discrimination, guess, slip) -> float:
+def item_response_function(difficulty: float, skill: float, discrimination: float, guess: float, slip: float) -> float:
     """
     Computes the logistic function for the given arguments and returns a float. The initial np.product is necessary for
     the multidimensional case.
@@ -24,7 +24,8 @@ def item_response_function(difficulty, skill, discrimination, guess, slip) -> fl
     )
 
 
-def inverse_item_response_function(difficulty, skill, discrimination, guess, slip) -> float:
+def inverse_item_response_function(
+        difficulty: float, skill: float, discrimination: float, guess: float, slip: float) -> float:
     """
     Computes 1 - logistic function for the given arguments and returns a float.
     :param difficulty: difficulty of the item
@@ -37,34 +38,36 @@ def inverse_item_response_function(difficulty, skill, discrimination, guess, sli
     return 1.0 - item_response_function(difficulty, skill, discrimination, guess, slip)
 
 
-def information_function(b, theta, discrimination, guess=0, slip=0) -> float:
+def information_function(
+        difficulty: float, skill: float, discrimination: float, guess: float = 0.0, slip: float = 0.0) -> float:
     """
-    Information function of a question: I(theta) = (P'(theta))**2/(P(theta)*Q(theta)), where Q(theta) = 1 - P(theta)
-    :param b: difficulty of the item
-    :param theta: the skill of the user
-    :param discrimination: the discrimination factor, mono-dimensional
-    :param guess: the guess factor, mono-dimensional
-    :param slip: the slip factor, mono-dimensional
+    Information function of a question: I(skill) = (P'(skill))**2/(P(skill)*Q(skill)), where Q(skill) = 1 - P(skill)
+    :param difficulty: difficulty of the item
+    :param skill: the skill of the user
+    :param discrimination: the discrimination factor
+    :param guess: the guess factor
+    :param slip: the slip factor
     :return: result of the Information Function
     """
     return np.divide(
-        np.square(derivative_item_response_function(b, theta, discrimination, guess, slip)),
+        np.square(derivative_item_response_function(difficulty, skill, discrimination, guess, slip)),
         (
-                item_response_function(b, theta, discrimination, guess, slip)
-                * inverse_item_response_function(b, theta, discrimination, guess, slip)
+                item_response_function(difficulty, skill, discrimination, guess, slip)
+                * inverse_item_response_function(difficulty, skill, discrimination, guess, slip)
         )
     )
 
 
-def derivative_item_response_function(b, theta, discrimination, guess, slip) -> float:
+def derivative_item_response_function(
+        difficulty: float, skill: float, discrimination: float, guess: float, slip: float) -> float:
     """
     Computes the derivative of the item_response_function.
-    :param b: difficulty of the item
-    :param theta: the skill of the user
-    :param discrimination: the discrimination factor, mono-dimensional
-    :param guess: the guess factor, mono-dimensional
-    :param slip: the slip factor, mono-dimensional
+    :param difficulty: difficulty of the item
+    :param skill: the skill of the user
+    :param discrimination: the discrimination factor
+    :param guess: the guess factor
+    :param slip: the slip factor
     :return: the result of the derivative of the item response function
     """
-    x = np.exp(-DISCRIMINATION_COEFFICIENT * discrimination * (theta[0]-b[0]))
+    x = np.exp(-DISCRIMINATION_COEFFICIENT * discrimination * (skill-difficulty))
     return np.divide((1.-guess-slip) * x * (-DISCRIMINATION_COEFFICIENT) * discrimination, np.square(1 + x**2))
