e459e6b547da1c8c12d17953a5c083aebe1f733f
Luca Benedetto <luca@benedettoluca.com>
2020-06-04 11:18:17 +0200

    add example script wrongness majority

--- scripts/README.md
+++ scripts/README.md
@@ -3,3 +3,6 @@
 * `example_r2de.py`: implementation of the R2DE model with *text2props*. [R2DE](https://arxiv.org/abs/2001.07569) 
 consists in a IRT estimation with a two-parameters model for the ground truth latent traits, followed by two Random 
 Forest models for the estimation of latent traits from textual information. 
+* `example_wrongness_majority.py`: implementation of a simple model that performs the estimation of the wrongness of 
+each question (i.e. fraction of wrong answers) and the estimation from text with a majority model (i.e. for all the 
+questions the same difficulty is predicted, and that is the average wrongness of all the training questions).
---
+++ scripts/example_wrongness_majority.py
@@ -0,0 +1,25 @@
+from text2props.model.literature_architectures import MajorityWrongnessText2PropsModel
+from text2props.constants import DATA_PATH
+import os
+import pandas as pd
+
+# TODO IMPORTANT: in order to run this script you have to provide your own data
+A_GTE = 'a_gte.csv'
+Q_TRAIN = 'q_train.csv'
+Q_TEST = 'q_test.csv'
+
+# load data
+df_gte = pd.read_csv(os.path.join(DATA_PATH, A_GTE))
+df_train = pd.read_csv(os.path.join(DATA_PATH, Q_TRAIN))
+df_test = pd.read_csv(os.path.join(DATA_PATH, Q_TEST))
+
+# define and train the text2props_model
+text2props_model = MajorityWrongnessText2PropsModel()
+text2props_model.train(df_train=df_train, df_gte=df_gte)
+
+# perform predictions
+predictions = text2props_model.predict(df_test)
+
+# evaluate model and print results
+results = text2props_model.compute_error_metrics_latent_traits_estimation(df_test)
+print(results)
