4113982af62a77dd3e9c583e27ee034034c5db16
Luca Benedetto <luca@benedettoluca.com>
2020-06-05 09:35:35 +0200

    update docstrings

--- text2props/modules/estimators_from_text/_feature_engineering_and_regression.py
+++ text2props/modules/estimators_from_text/_feature_engineering_and_regression.py
@@ -2,36 +2,71 @@ from ._base import BaseEstimatorFromText
 from text2props.modules.feature_engineering import FeatureEngineeringModule
 from text2props.modules.regression import RegressionModule
 from text2props.constants import Q_ID
+import pandas as pd
 
 
 class FeatureEngAndRegressionEstimatorFromText(BaseEstimatorFromText):
 
     def __init__(self, pipelines: dict):
         super().__init__()
-        self.pipelines = pipelines
+        self.pipelines = pipelines  # one pipeline for each latent trait
 
-    def train(self, df_train, ground_truth_latent_traits):
+    def train(self, df_train: pd.DataFrame, ground_truth_latent_traits: dict):
+        """
+        Trains the EstimatorFromText object, training all the pipelines contained in the object.
+        :param df_train:
+        :param ground_truth_latent_traits:
+        :return:
+        """
         for latent_trait in self.pipelines.keys():
             local_y = [ground_truth_latent_traits[latent_trait][q_id] for q_id in df_train[Q_ID].values]
             self.pipelines[latent_trait].train(df_train, local_y)
 
-    def predict(self, input_df):
+    def predict(self, input_df: pd.DataFrame):
+        """
+        Performs the prediction. The returned object is a dictionary whose keys are the names of the latent traits.
+        :param input_df:
+        :return:
+        """
         predictions = dict()
         for latent_trait in self.pipelines.keys():
             predictions[latent_trait] = self.pipelines[latent_trait].predict(input_df)
         return predictions
 
     def randomized_cv_train(
-            self, param_distributions, df_train, ground_truth_latent_traits,
-            n_iter=10, n_jobs=None, cv=None, random_state=None
+            self,
+            param_distributions: dict,
+            df_train: pd.DataFrame,
+            ground_truth_latent_traits: dict,
+            n_iter: int = 10,
+            n_jobs: int = None,
+            cv: int = None,
+            random_state: int = None,
     ):
+        """
+        Trains the EstimatorFromText object with RandomizedCV, bu training all the pipelines, and returns the scores so
+        that they can be compared with the results obtained with other models.
+        :param param_distributions:
+        :param df_train:
+        :param ground_truth_latent_traits:
+        :param n_iter:
+        :param n_jobs:
+        :param cv:
+        :param random_state:
+        :return:
+        """
         scores = []
         for latent_trait in self.pipelines.keys():
             local_y = [ground_truth_latent_traits[latent_trait][q_id] for q_id in df_train[Q_ID].values]
             scores.append(
                 self.pipelines[latent_trait].randomized_cv_train(
-                    param_distributions=param_distributions[latent_trait], df_train=df_train, y_train=local_y,
-                    n_iter=n_iter, n_jobs=n_jobs, cv=cv, random_state=random_state
+                    param_distributions=param_distributions[latent_trait],
+                    df_train=df_train,
+                    y_train=local_y,
+                    n_iter=n_iter,
+                    n_jobs=n_jobs,
+                    cv=cv,
+                    random_state=random_state
                 )
             )
         return scores
