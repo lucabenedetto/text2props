252b4aca94c8d91e58958e3c7be42e7b186e1d7c
Luca Benedetto <luca@benedettoluca.com>
2020-06-10 11:44:03 +0200

    update docstrings

--- text2props/modules/regression/_module.py
+++ text2props/modules/regression/_module.py
@@ -9,23 +9,50 @@ from .components import BaseRegressionComponent
 class RegressionModule(object):
 
     def __init__(self, components: List[BaseRegressionComponent] = None):
+        """
+        Initialize RegressionModule object. If available components are initialized, otherwise creates sn empty list
+        :param components: the list of RegressionComponent objects that the module is made of (possibly None).
+        """
         if components is None:
             self.components = []
         else:
             self.components = components
 
     def add_component(self, component: BaseRegressionComponent):
+        """
+        Add a RegressionComponent to the RegressionModule object
+        :param component:
+        :return:
+        """
         self.components.append(component)
 
     def add_list_components(self, list_components: List[BaseRegressionComponent]):
+        """
+        Add a list of RegressionComponent objects to the RegressionModule
+        :param list_components:
+        :return:
+        """
         for component in list_components:
             self.components.append(component)
 
     def train(self, x: coo_matrix, y: List[float]):
+        """
+        Train the RegressionModule, by training all the RegressionComponent objects
+        :param x: sparse matrix (obtained from the FeatureEngineering module) containing the features obtained from text
+        :param y: iterable containing target values
+        :return:
+        """
         for component in self.components:
             component.train(x, y)
 
     def predict(self, x: coo_matrix):
+        """
+        Performs the prediction for the sparse matrix given as input. The matrix is obtained from the FeatureEngineering
+          module and contains the features obtained transforming the input text. The final prediction for each input
+          sample is obtained by averaging the prediction of all the components of the RegressionModule.
+        :param x:
+        :return:
+        """
         partial_results = []
         for component in self.components:
             partial_results.append(component.predict(x))
@@ -42,8 +69,8 @@ class RegressionModule(object):
             random_state: int
     ) -> float:
         """
-        Performs the training with randomized cross validation of all the components in the Regression module. For each
-        component, computes the R2 score of the best performing configuration. In the end, it returns the R2 score
+        Performs the training with randomized cross validation of all the components in the RegressionModule. For each
+        RegressionComponent, computes the R2 score of the best performing configuration. In the end returns the R2 score
         obtained as the average of the R2 scores of all the components.
         :param x_train:
         :param y_train:
