fdc795efc051c669f72b2612b59d8204c9e284f2
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-02 17:39:08 +0000

    update linguistic module with new features

--- text2props/modules/feature_engineering/components/_linguistic.py
+++ text2props/modules/feature_engineering/components/_linguistic.py
@@ -1,5 +1,7 @@
+import nltk.data
 import numpy as np
 import pandas as pd
+import string
 import textstat
 from typing import Optional
 from scipy.sparse import coo_matrix
@@ -16,12 +18,20 @@ SENTENCE_COUNT_DISTRACTORS = 'sentence_count_wrong_choices'
 AVG_WORD_LENGTH_QUESTION = 'avg_word_len_question'
 RATIO_LENGTH_QUESTION_ANSWERS = 'ratio_len_question_correct_choices'
 RATIO_LEN_QUESTION_DISTRACTORS = 'ratio_len_question_wrong_choices'
+CHARACTER_COUNT_QUESTION = 'character_count_question'
+AVG_SENTENCE_LENGTH = 'avg_sentence_length'
+NUM_LONG_WORDS = 'num_long_words'
+NUM_SHORT_WORDS = 'num_short_words'
+AVG_NUM_LONG_WORDS_PER_SENTENCE = 'avg_num_long_words_per_sentence'
+AVG_NUM_SHORT_WORDS_PER_SENTENCE = 'avg_num_short_words_per_sentence'
+AVG_NOUNS_PER_SENTENCE = 'avg_nouns_per_sentence'
+AVG_VERBS_PER_SENTENCE = 'avg_verbs_per_sentence'
 
 
 class LinguisticFeaturesComponent(BaseFeatEngComponent):
 
-    def __init__(self):
-        return
+    def __init__(self, version: Optional[int] = 1):
+        self.version = version
 
     def fit_transform(self, input_df: pd.DataFrame) -> coo_matrix:
         """
@@ -50,11 +60,15 @@ class LinguisticFeaturesComponent(BaseFeatEngComponent):
         df = pd.DataFrame()
 
         for q_id, q_text in input_df[[Q_ID, Q_TEXT]].values:
+            q_text_no_punctuation = q_text.translate(str.maketrans('', '', string.punctuation))
+            list_words = q_text_no_punctuation.split(' ')
             answers_text = correct_ans_text_dict[q_id]
             distractors_text = wrong_ans_text_dict[q_id]
+
             lexicon_count_question = textstat.lexicon_count(q_text)
             lexicon_count_answers = np.mean([textstat.lexicon_count(x) for x in answers_text])
             lexicon_count_distractors = np.mean([textstat.lexicon_count(x) for x in distractors_text])
+
             new_row = pd.DataFrame([{
                 LEXICON_COUNT_QUESTION: lexicon_count_question,
                 LEXICON_COUNT_ANSWERS: lexicon_count_answers,
@@ -62,10 +76,42 @@ class LinguisticFeaturesComponent(BaseFeatEngComponent):
                 SENTENCE_COUNT_QUESTION: textstat.sentence_count(q_text),
                 SENTENCE_COUNT_ANSWERS: np.mean([textstat.sentence_count(x) for x in answers_text]),
                 SENTENCE_COUNT_DISTRACTORS: np.mean([textstat.sentence_count(x) for x in distractors_text]),
-                AVG_WORD_LENGTH_QUESTION: np.mean([len(x) for x in q_text.split(' ')]),  # todo possibly improve this
+                AVG_WORD_LENGTH_QUESTION: np.mean([len(x) for x in list_words]),
                 RATIO_LENGTH_QUESTION_ANSWERS: (1 + lexicon_count_question) / (1 + lexicon_count_answers),
                 RATIO_LEN_QUESTION_DISTRACTORS: (1 + lexicon_count_question) / (1 + lexicon_count_distractors),
             }])
             df = pd.concat([df, new_row], ignore_index=True)
 
+            if self.version > 1:
+                tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+                list_sentences_question = tokenizer.tokenize(q_text)
+
+                nltk.download('averaged_perceptron_tagger')
+                words_list = nltk.word_tokenize(q_text)
+                tagged_words_list = nltk.pos_tag(words_list)
+
+                # number of characters
+                new_row[CHARACTER_COUNT_QUESTION] = len(q_text)
+                # average sentence length
+                new_row[AVG_SENTENCE_LENGTH] = np.mean([len(sentence) for sentence in list_sentences_question])
+
+                # nouns per sentence
+                new_row[AVG_NOUNS_PER_SENTENCE] = np.mean([len([x for x in nltk.pos_tag(nltk.word_tokenize(s)) if x[1] in {'NN', 'NNS'}]) for s in list_sentences_question])
+                # verbs per sentence
+                new_row[AVG_VERBS_PER_SENTENCE] = np.mean([len([x for x in nltk.pos_tag(nltk.word_tokenize(s)) if x[1] in {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}]) for s in list_sentences_question])
+
+                # Number of long words (7 characters or more) in the question
+                new_row[NUM_LONG_WORDS] = len([x for x in list_words if len(x) >= 7])
+                # Number of short words in the question
+                new_row[NUM_SHORT_WORDS] = len([x for x in list_words if len(x) < 7])
+                # Average number of long words per sentence
+                new_row[AVG_NUM_LONG_WORDS_PER_SENTENCE] = np.mean([len([w for w in s if len(w) >= 7]) for s in list_sentences_question])
+                # Average number of short words per sentence
+                new_row[AVG_NUM_SHORT_WORDS_PER_SENTENCE] = np.mean([len([w for w in s if len(w) < 7]) for s in list_sentences_question])
+
         return coo_matrix(df.values)
+
+# other features that might be added in the futures;
+#   - Average number of syllables per word
+#   - number of double consonants
+#   - number of vowels
